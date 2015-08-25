9612dee2c8af80dc4216ab15c869d2ef69333af4
Zeynep Akkalyoncu <zeynep.akkalyoncu@metu.edu.tr>
2015-08-25 14:16:31 +0300

    Update Transformation.cpp

--- Transformation.cpp
+++ Transformation.cpp
@@ -6,32 +6,34 @@ Transformation::Transformation(){
 	
 }
 
+float Transformation::integrate(){
+	
+	
+}
+
 // TODO: check positive/negative orientations
-float** Transformation::getTransformationMatrix(float* angularVelocity){
-	float gyro_x = getGyrox(angularVelocity);
-	float gyro_y = getGyroy(angularVelocity);
-	float gyro_z = getGyroz(angularVelocity);
-	float alpha = getRotationAngle(gyro_x);
-	float beta = getRotationAngle(gyro_y);
-	float gamma = getRotationAngle(gyro_z);
-	float[3][3] matrix = {{cos(beta)*cos(gamma), -sin(gamma), sin(beta)},
+double** Transformation::getTransformationMatrix(double gyroXrate, double gyroYrate, double gyroZrate, double dt){
+	double alpha = getRotationAngle(gyroXrate, dt);
+	double beta = getRotationAngle(gyroYrate, dt);
+	double gamma = getRotationAngle(gyroZrate, dt);
+	double[3][3] matrix = {{cos(beta)*cos(gamma), -sin(gamma), sin(beta)},
 						{sin(gamma), cos(alpha)*cos(gamma), -sin(alpha)},
 						{-sin(beta), sin(alpha), cos(alpha)*cos(beta)}};
 	return matrix;
 }
 
-float Transformation::getRotationAngle(float angularVelocity){
-	float angle = integrate(angularVelocity, dt);
+double Transformation::getRotationAngle(double angularVelocity double dt){
+	double angle = integrate(angularVelocity, dt); // integrate parameters missing
 	return angle;
 }
 
-float* Transformation::transformToNormal(float* relativeAcceleration, float* angularVelocity){
-	float** transformationMatrix = getTransformation(angularVelocity);
-	float absoluteAcceleration[3];
+double* Transformation::transformToNormal(double kalAngleX, double kalAngleY, double kalAngleZ, double gyroXrate, double gyroYrate, double gyroZrate, double dt){
+	double** transformationMatrix = getTransformationMatrix(angularVelocity);
+	double absoluteAcceleration[3];
 	for (int i = 0; i < 3; i++){
-		absoluteAcceleration[i] = absoluteAcceleration[0] * transformationMatrix[0][i]
-								+ absoluteAcceleration[1] * transformationMatrix[1][i]
-								+ absoluteAcceleration[2] * transformationMatrix[2][i];	
+		absoluteAcceleration[i] = kalAngleX * transformationMatrix[0][i]
+								+ kalAngleY * transformationMatrix[1][i]
+								+ kalAngleZ * transformationMatrix[2][i];	
 	}
 	cancelGravity(absoluteAcceleration);
 	return absoluteAcceleration;
